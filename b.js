5f13d9ab0dcb6e6e55188e44d0bfa8d13a69cacc
Velkur Dharani <dharanivelkur@gmail.com>
2023-03-22 12:58:40 +0530

    added 2 more answers

--- b.js
+++ b.js
@@ -1,3 +1,27 @@
+/*----------1st question----------*/
 let obj1={name:"Person 1",age:5};
 let obj2={age:5,name:"Person 1"};
 console.log(JSON.stringify(obj1)===JSON.stringify(obj2))
+
+
+/*----------2nd question----------*/
+let promise=fetch("https://restcountries.com/v3.1/all");
+promise.then((response)=>{
+    return response.json();
+}).then(value=>{
+    for(i=0;i<value.length;i++){
+        console.log(value[i].flags);;
+    }
+});
+
+
+/*----------3rd question----------*/
+let promise1=fetch("https://restcountries.com/v3.1/all");
+promise1.then((response)=>{
+    return response.json();
+}).then(value=>{
+    for(i=0;i<value.length;i++){
+        console.log(value[i].name.common,value[i].population,value[i].region,value[i].subregion);;
+    }
+});
+
