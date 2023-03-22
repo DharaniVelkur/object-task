d3ee086f83cf2cd882a89d63bc3da1b85600b7a6
Velkur Dharani <dharanivelkur@gmail.com>
2023-03-22 11:23:47 +0530

    added JSON

--- a.js
+++ a.js
@@ -1,11 +1,13 @@
 
-let obj={
-    fname:"dharani",
-    lname:"velkur",
-    fruits_I_like:["apple","pineapple","guava","orange","banana","kiwi","strawberry","watermelon","grapes","pomegranate",[1,2,3]],
-    inst:"guvi",
-    roll_no:123
-}
-for(i=0;i<obj.fruits_I_like.length;i++){
-    console.log(obj.fruits_I_like[i]);
-}
+// let obj={
+//     fname:"dharani",
+//     lname:"velkur",
+//     fruits_I_like:["apple","pineapple","guava","orange","banana","kiwi","strawberry","watermelon","grapes","pomegranate",[1,2,3]],
+//     inst:"guvi",
+//     roll_no:123
+// }
+// for(i=0;i<obj.fruits_I_like.length;i++){
+//     console.log(obj.fruits_I_like[i]);
+// }
+
+
